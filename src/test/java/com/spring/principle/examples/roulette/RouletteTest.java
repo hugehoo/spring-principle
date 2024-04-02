1374467ab921e4cb0b9f309bd3d296b4f87d8db7
hoo <tbnsok40@gmail.com>
2024-04-02 23:41:20 +0900

    test: 룰렛 게임 테스트

--- src/test/java/com/spring/principle/examples/roulette/RouletteTest.java
+++ src/test/java/com/spring/principle/examples/roulette/RouletteTest.java
@@ -2,22 +2,32 @@ package com.spring.principle.examples.roulette;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class RouletteTest {
-    // 요구사항
-    // probability 는 합쳐서 1이 돼야한다.
+
     @Test
-    void sample() {
-        List<Roulette> roulettes = List.of(new Roulette(10, 0.5, 40),
-            new Roulette(100, 0.3, 30),
-            new Roulette(500, 0.2, 16),
-            new Roulette(600, 0.05, 8),
-            new Roulette(900, 0.03, 4),
-            new Roulette(1000, 0.02, 2));
+    void playTest() {
+        List<Roulette> initialRoulette = List.of(
+            new Roulette(10, BigDecimal.valueOf(0.5), 40),
+            new Roulette(50, BigDecimal.valueOf(0.3), 30),
+            new Roulette(100, BigDecimal.valueOf(0.2), 16),
+            new Roulette(500, BigDecimal.valueOf(0.05), 8),
+            new Roulette(800, BigDecimal.valueOf(0.03), 4),
+            new Roulette(1000, BigDecimal.valueOf(0.02), 2));
 
+        RouletteGame rouletteGame = new RouletteGame(initialRoulette);
+        for (int i = 0; i < 100; i++) {
+            rouletteGame.play();
+        }
+
+        List<Roulette> rouletteList = rouletteGame.getRouletteList();
+        for (Roulette roulette : rouletteList) {
+            Assertions.assertThat(roulette.getStocks()).isEqualTo(0);
+        }
     }
-
 }
